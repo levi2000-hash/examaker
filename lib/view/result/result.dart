07286d86a3942c35ed7bf86a6c205160ca196250
Omarofskii <omar_rahman007hotmail.com>
2022-05-14 13:54:07 +0200

    result appBar

--- lib/view/result/result.dart
+++ lib/view/result/result.dart
@@ -12,16 +12,20 @@ class _ResultsState extends State<Results> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+          title: const Text("Resultaten"),
+        ),
       body: Container(
         height: MediaQuery.of(context).size.height,
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-            Text("${widget.juistA}/${widget.totaal}"),
+            Text("${widget.juistA}/${widget.totaal}", style: TextStyle(fontSize: 25),),
             SizedBox(height: 8,),
             Text("U hebt ${widget.juistA} juiste "
-            "en ${widget.foutA} antwoorden beantwoord")
+            "en ${widget.foutA} antwoorden beantwoord", style: TextStyle(fontSize: 15, color: Colors.grey),
+            textAlign: TextAlign.center,)
 
           ],),
           )
